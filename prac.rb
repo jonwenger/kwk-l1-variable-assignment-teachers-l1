ae5a92ea4c737f29f617526104127dff4c9057d2
Jon Wenger <jonwenger783@gmail.com>
2018-08-06 18:23:30 +0000

    automatically backed up by learn

--- prac.rb
+++ prac.rb
@@ -2,4 +2,4 @@ first_name = "Jon"
 last_name = "Wenger"
 age = 25
 # String Interpolation
-puts "My first name is #{first_name} and i am #{age.to_s}"
+puts "My first name is first_name and i am #{age.to_s}"

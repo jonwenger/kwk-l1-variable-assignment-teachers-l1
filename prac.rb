a386b020583d622b9b83889f672f634c67de9e1b
Jon Wenger <jonwenger783@gmail.com>
2018-08-06 18:18:50 +0000

    automatically backed up by learn

--- prac.rb
+++ prac.rb
@@ -1,3 +1,4 @@
 first_name = "Jon"
 last_name = "Wenger"
-puts "My name is " + first_name + " " + last_name
+# String Interpolation
+puts "My first name is #{first_name} and my last name is #{last_name}"
